d0bbf5cde2da116e5ed1de9301023b7f18cb8795
Tom Hill <tomhill98@me.com>
2020-07-18 23:54:29 +1000

    skrrt skrrrt working on frontend bro

--- frontend/pages/calendar.js
+++ frontend/pages/calendar.js
@@ -1,6 +1,12 @@
 import Cal from "../components/Cal/Cal";
 
+import { useContext } from "react";
+import UserContext from "../context/auth";
+
 export default function Calendar() {
+  // const [user, setUser] = useContext(UserContext);
+  const user = { name: "Tom Hill" };
+
   const peeps = [
     { name: "Tom Shortay", days: ["monday", "tuesday", "wednesday"] },
     { name: "Rohan Maloney", days: ["monday", "tuesday", "wednesday"] },
@@ -9,10 +15,18 @@ export default function Calendar() {
   ];
 
   return (
-    <div className="columns is-centered">
-      <div className="column is-10 box">
-        <Cal people={peeps} />
+    <>
+      <h3 className="title is-4">
+        Hi there, {user ? user.name.split(" ")[0] : "Not logged in!"}.
+        <br />
+        <br />
+        Find a spot at the office.
+      </h3>
+      <div className="columns is-centered">
+        <div className="column is-10 box">
+          <Cal people={peeps} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
