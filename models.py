0bae7756424ab95bbb5c9e6f5265a50201bafc62
Tom Wright <tdcwright1@gmail.com>
2020-07-18 14:04:30 +1000

    Creates initial models

--- models.py
+++ models.py
@@ -11,8 +11,7 @@ if "HEROKU" in os.environ:
         host=url.hostname,
         port=url.port,
     )
-else:
-    
+else: 
     import getpass
     username = getpass.getuser()
     if (username == "twright" or username == "tdcwr" or username == "tomhill"):
@@ -24,3 +23,42 @@ else:
         db_user = os.environ["DB_USER"]
         db_pword = os.environ["DB_PASSWORD"]
         db = PostgresqlDatabase(db_name, user=db_user, password=db_pword)
+
+# Base Peewee class
+class Base(Model):
+    class Meta:
+        database=db
+
+# Stores all offices (can be multiple buildings)
+class CompanyBuildings(Base):
+    pass
+
+# stores all desk-spaces and conference rooms
+class Office(Base):
+    pass
+
+# a set of desks
+class Space(Base):
+    pass
+
+class Desk(Base):
+    pass
+
+class Person(Base):
+    isPm = True
+    personID = 123
+
+class Project(Base):
+    pass
+
+class JiraTicket(Base):
+    pass
+
+class ProjectSpaceAllocation(Base):
+    pass
+
+class AllocatedDesk(Base):
+    pass
+
+class PersonTickets(Base):
+    pass
