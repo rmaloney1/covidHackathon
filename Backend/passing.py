d8eeb387d5e6abb73571bbf8004842c2f5acbb2a
Tom Hill <tomhill98@me.com>
2020-07-19 07:04:35 +1000

    calendar 200 ok

--- Backend/passing.py
+++ Backend/passing.py
@@ -1,7 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, abort
 import json
-from models import CompanyBuildings, Person, Project, JiraTicket, PersonTickets, MeetingRequest, model_to_dict, apiUser, ourProject
+from models import (
+    CompanyBuildings,
+    Person,
+    Project,
+    JiraTicket,
+    PersonTickets,
+    MeetingRequest,
+    model_to_dict,
+    apiUser,
+    ourProject,
+)
 from processing import contactTrace
 
 me = apiUser("rohanmaloney@outlook.com", "lxZVdyemldyTFkmwM5Hn94BD")
@@ -10,9 +20,10 @@ auth = me.getAuth()
 app = Flask(__name__)
 api = Api(app)
 
-# data 
+# data
 # with open("..json") as f:
 #     data = json.load(f)
+
 
 class user(Resource):
     def post(self):
@@ -27,12 +38,11 @@ class user(Resource):
         print("hi")
         email = request.args["email"]
         try:
-            person = Person.select().where(Person.email==email).get()
+            person = Person.select().where(Person.email == email).get()
         except:
             return abort(404)
 
-        return {"name" : person.personID, "email" : email}
-        
+        return {"name": person.personID, "email": email}
 
 
 class tasks(Resource):
@@ -46,13 +56,15 @@ class tasks(Resource):
             details["name"] = task.name
             print("name of", task.ticketID, "is", task.name)
             details["status"] = "in person" if task.assigned else "unassigned"
-            attendees = PersonTickets.select().where(PersonTickets.ticketID==task.ticketID)
+            attendees = PersonTickets.select().where(
+                PersonTickets.ticketID == task.ticketID
+            )
             names = [i.person.personID for i in attendees]
             details["attendees"] = names
             verbose.append(details)
 
         return verbose
-    
+
     def post(self):
         # name and email
         data = request.json
@@ -61,37 +73,52 @@ class tasks(Resource):
         dueDate = data["dueDate"]
         priority = data["priority"]
 
-        MeetingRequest.makeRequest(ticketID, afterDate,  dueDate, priority)
+        MeetingRequest.makeRequest(ticketID, afterDate, dueDate, priority)
         return {}
+
 
 class calendar(Resource):
     def get(self):
         output = []
         # A list of all people that have a allocated task
-        fullPersonTaskList = PersonTickets.select().join(MeetingRequest).where(MeetingRequest.requestFilled == True)
+        fullPersonTaskList = (
+            PersonTickets.select()
+            .join(
+                MeetingRequest, on=(MeetingRequest.ticketID == PersonTickets.ticketID)
+            )
+            .where(MeetingRequest.requestFilled == True)
+        )
         for personTask in fullPersonTaskList.iterator():
             found = False
-            taskSummary = {"name":personTask.ticketID.name,"attendees": [t for t in PersonTickets.select().where(PersonTickets.person == personTask.person)], "day": (personTask.dateAllocated).strftime("%m/%d/%Y")}
+            taskSummary = {
+                "name": personTask.ticketID.name,
+                "attendees": [
+                    t
+                    for t in PersonTickets.select().where(
+                        PersonTickets.person == personTask.person
+                    )
+                ],
+                "day": (personTask.dateAllocated).strftime("%m/%d/%Y"),
+            }
             for i in output:
                 if output[i]["Name"] == personTask.person:
                     found = True
                     output[i]["meetings"].append(taskSummary)
-            
-            if not found:
-                output.append({"Name":personTask.person, "meetings":[taskSummary]})
-        return output
 
+            if not found:
+                output.append({"Name": personTask.person, "meetings": [taskSummary]})
+        return output
 
 
 class trace(Resource):
     def get(self, p_id):
         return contactTrace(p_id)
 
-api.add_resource(user, '/user')
-api.add_resource(tasks, '/tasks')
-api.add_resource(calendar, '/calendar')
-api.add_resource(trace, '/trace/<string:p_id>')
 
-if __name__ == '__main__':
-    app.run(debug = True)
+api.add_resource(user, "/user")
+api.add_resource(tasks, "/tasks")
+api.add_resource(calendar, "/calendar")
+api.add_resource(trace, "/trace/<string:p_id>")
 
+if __name__ == "__main__":
+    app.run(debug=True)
